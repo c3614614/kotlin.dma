3d95aeebe31e5d875135da1f5401aaa09203ad52
Diallo <M.Diallo2774@student.leedsbeckett.ac.uk>
2024-03-05 19:49:24 +0000

    added the a new text composable that accept text arg set to the from value

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -33,11 +33,15 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     Text (
         text = message,
         fontSize = 100.sp,
         lineHeight = 116.sp,
+    )
+    Text(
+        text = from,
+        fontSize = 36.sp
     )
 }
 
@@ -45,7 +49,7 @@ fun GreetingText(message: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Lekalon")
+        GreetingText(message = "Happy Birthday Lekalon", from ="From Emma")
 
     }
 }
