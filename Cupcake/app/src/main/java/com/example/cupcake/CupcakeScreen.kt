ee3e5149ae2b741628c02bee0067f03dcc275cb4
lk <lk@gmail.com>
2024-03-16 17:30:21 +0000

    Refactored the AppBar to respond dynamically to navigation by implementing dynamic title updates and showing the Up button based on navigation state

--- Cupcake/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ Cupcake/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -19,20 +19,17 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.cupcake.ui.OrderViewModel
 import com.example.cupcake.ui.OrderSummaryScreen
@@ -45,33 +42,32 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.platform.LocalContext
 import com.example.cupcake.data.DataSource.flavors
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.ui.platform.LocalContext
 import android.content.Context
 import android.content.Intent
-
+import androidx.annotation.StringRes
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 
 /**
  * Composable that displays the topBar and displays back button if back navigation is possible.
  */
 
-enum class CupcakeScreen {
-    Start,
-    Flavor,
-    Pickup,
-    Summary
+enum class CupcakeScreen(@StringRes val title: Int) {
+    Start(title = R.string.app_name),
+    Flavor(title = R.string.choose_flavor),
+    Pickup(title = R.string.choose_pickup_date),
+    Summary(title = R.string.order_summary)
 }
 
 @Composable
 fun CupcakeAppBar(
+    currentScreen: CupcakeScreen,
     canNavigateBack: Boolean,
     navigateUp: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     TopAppBar(
-        title = { Text(stringResource(id = R.string.app_name)) },
-        colors = TopAppBarDefaults.mediumTopAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer
-        ),
+        title = { Text(stringResource(currentScreen.title)) },
         modifier = modifier,
         navigationIcon = {
             if (canNavigateBack) {
@@ -90,13 +86,20 @@ fun CupcakeAppBar(
 fun CupcakeApp(
     viewModel: OrderViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
-) {
+){
+
+    val backStackEntry by navController.currentBackStackEntryAsState()
+
+    val currentScreen = CupcakeScreen.valueOf(
+        backStackEntry?.destination?.route ?: CupcakeScreen.Start.name
+    )
 
     Scaffold(
         topBar = {
             CupcakeAppBar(
-                canNavigateBack = false,
-                navigateUp = { /* TODO: implement back navigation */ }
+                currentScreen = currentScreen,
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() }
             )
         }
     ) { innerPadding ->
@@ -162,6 +165,10 @@ fun CupcakeApp(
         }
     }
 }
+
+
+
+
 private fun cancelOrderAndNavigateToStart(
     viewModel: OrderViewModel,
     navController: NavHostController
