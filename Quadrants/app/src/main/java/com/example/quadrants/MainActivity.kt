c9122dafe0be76c9f56bb3453b3f8efcfbfb7275
lk <lk@gmail.com>
2024-03-17 01:07:10 +0000

    Defined ComposableInfoCard composable with a Column layout presenting title and description texts, styled with specified modifier attributes.

--- Quadrants/app/src/main/java/com/example/quadrants/MainActivity.kt
+++ Quadrants/app/src/main/java/com/example/quadrants/MainActivity.kt
@@ -3,17 +3,24 @@ package com.example.quadrants
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.quadrants.ui.theme.QuadrantsTheme
 import com.example.quadrants.R
 
@@ -78,7 +85,24 @@ private fun ComposableInfoCard(
     modifier: Modifier = Modifier
 )
 {
-
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 
