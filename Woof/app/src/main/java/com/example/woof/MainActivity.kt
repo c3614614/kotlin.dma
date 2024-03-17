07d9648374f44b5566f8dff5557204228b82359f
lk <lk@gmail.com>
2024-03-17 02:12:47 +0000

    Added a clip attribute to the Image modifier within the DogIcon composable, using MaterialTheme.shapes.small to clip the image into a circular shape.

--- Woof/app/src/main/java/com/example/woof/MainActivity.kt
+++ Woof/app/src/main/java/com/example/woof/MainActivity.kt
@@ -31,11 +31,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -43,6 +45,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.woof.data.Dog
 import com.example.woof.data.dogs
 import com.example.woof.ui.theme.WoofTheme
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -114,7 +118,10 @@ fun DogItem(
         Image(
             modifier = modifier
                 .size(dimensionResource(R.dimen.image_size))
-                .padding(dimensionResource(R.dimen.padding_small)),
+                .padding(dimensionResource(R.dimen.padding_small))
+                .clip(MaterialTheme.shapes.small),
+            contentScale = ContentScale.Crop,
+
             painter = painterResource(dogIcon),
 
             // Content Description is not needed here - image is decorative, and setting a null content
