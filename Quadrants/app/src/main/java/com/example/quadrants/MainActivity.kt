cd1ae707cbfbfcf8e8dcc7a89c22f725f78e3600
lk <lk@gmail.com>
2024-03-17 00:47:15 +0000

    Added MainPage composable function with two ComposableInfoCard elements in a Row with background color, title and description alined vertically

--- Quadrants/app/src/main/java/com/example/quadrants/MainActivity.kt
+++ Quadrants/app/src/main/java/com/example/quadrants/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.quadrants
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.quadrants.ui.theme.QuadrantsTheme
+import com.example.quadrants.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,8 +36,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainPage(modifier: Modifier = Modifier) {
+    Column(Modifier.fillMaxSize()) {
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(R.string.first_title),
+                description = stringResource(R.string.first_description),
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.second_title),
+                description = stringResource(R.string.second_description),
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
 
+
+
+
+    }
 }
+
+
+
+
 
 
 
