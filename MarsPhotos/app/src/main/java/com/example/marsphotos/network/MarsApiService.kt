ab9ec2f41a3f466946771dc641ebf452b5d6e7d3
lk <lk@gmail.com>
2024-03-18 00:47:28 +0000

    Added MarsApiService interface with Retrofit for connecting to the Mars web service and fetching JSON responses as Strings.

---
+++ MarsPhotos/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -0,0 +1,22 @@
+package com.example.marsphotos.network
+
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+// Base URL for the Mars web service
+private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com"
+
+// Retrofit object to communicate with the web service
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+
+interface MarsApiService {
+
+    @GET("photos")
+    fun getPhotos(): String
+}
