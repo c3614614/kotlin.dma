e1b8f118263bd768aeadf48abf774d02d07fa024
lk <lk@gmail.com>
2024-03-18 00:57:36 +0000

    Implemented lazy initialisation for Retrofit service in MarsApi object for improved efficiency.

--- MarsPhotos/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ MarsPhotos/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -1,6 +1,5 @@
 package com.example.marsphotos.network
 
-import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
@@ -14,9 +13,13 @@ private val retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
     .build()
 
-
 interface MarsApiService {
-
     @GET("photos")
     fun getPhotos(): String
+}
+
+object MarsApi {
+    val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
 }
