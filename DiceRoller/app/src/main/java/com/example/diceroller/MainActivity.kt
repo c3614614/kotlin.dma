e8a5ea834ac7b084d32fbdcf75956217f2b8ae70
lk <lk@gmail.com>
2024-05-10 01:54:47 +0100

    Add second dice roll functionality, including state retention, dynamic image resource determination, enhanced layout with row display, double roll message, and simultaneous roll handling.

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
@@ -12,19 +11,22 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller.ui.theme.DiceRollerTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-
-
-
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,7 +50,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var result by remember { mutableIntStateOf(1) }
+    var result by rememberSaveable { mutableStateOf( 1) }
+    var result2 by rememberSaveable { mutableStateOf( 1) }
+
 
     val imageResource = when (result) {
         1 -> R.drawable.dice_1
@@ -59,18 +63,48 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         else -> R.drawable.dice_6
     }
 
+    val imageResource2 = when (result2) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
     Column (
         modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(painter = painterResource(id = imageResource), contentDescription = result.toString())
-        Button(onClick = { result = (1..6).random() }) {
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        Row{
+
+            Image(
+                painter = painterResource(imageResource), contentDescription = result.toString())
+
+            Image(
+                painter = painterResource(imageResource2), contentDescription = result.toString())
+
+
+        }
+
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            result = (1..6).random()
+            result2 = (1..6).random()
+        }
+
+        ){
             Text(stringResource(R.string.roll))
+
+        }
+        if (result == result2){
+            Text("You rolled a double $result")
         }
     }
 }
-
-
+//preview
 @Preview(showBackground = true)
 @Composable
 fun DiceRollerApp() {
