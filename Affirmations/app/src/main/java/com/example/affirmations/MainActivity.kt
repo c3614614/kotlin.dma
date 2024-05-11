0dbd135b08c7c5e531cfc1038074fb5426694eeb
lk <lk@gmail.com>
2024-05-12 00:04:58 +0100

    changed the affirmation card and List to display a Toast message when a card is clicked, and ensuring that the Toast message remains visible until a dialog associated with the card is dismissed using showToast variable and the onClick parameter which represents the action to be performed when the card is clicked. Also Added a clickable modifier to the Card composable, which triggers the onClick lambda when the card is clicked

--- Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -1,30 +1,45 @@
 package com.example.affirmations
-import com.example.affirmations.model.Affirmation
+
+//import Datasource
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.affirmations.ui.theme.AffirmationsTheme
-import androidx.compose.material3.Card
-import androidx.compose.foundation.Image
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.material3.Text
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.affirmations.model.Affirmation
+import com.example.affirmations.ui.theme.AffirmationsTheme
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.foundation.shape.RoundedCornerShape
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.example.affirmations.data.Datasource
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,29 +56,26 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+
 }
 
 @Composable
 fun MainPage() {
-    affirmationList(
-        affirmationList = Datasource().loadAffirmations(),
-    )
+    val affirmations = Datasource().loadAffirmations()
+    affirmationList(affirmationList = affirmations)
 }
 @Composable
-fun affirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
-    LazyColumn(modifier = modifier) {
-        items(affirmationList) { affirmation ->
-            AffirmationCard(
-                affirmation = affirmation,
-                modifier = Modifier.padding(8.dp)
-            )
-        }
-    }
-}
+private fun AffirmationCard(
+    affirmation: Affirmation,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    val context = LocalContext.current
 
-@Composable
-fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
-    Card(modifier = modifier) {
+    Card(modifier = modifier.clickable { onClick() }
+
+    ) {
         Column {
             Image(
                 painter = painterResource(affirmation.imageResourceId),
@@ -76,13 +88,57 @@ fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
             Text(
                 text = LocalContext.current.getString(affirmation.stringResourceId),
                 modifier = Modifier.padding(16.dp),
-                style = MaterialTheme.typography.headlineSmall
-            )
+                style = MaterialTheme.typography.headlineSmall,
+
+                )
+
 
         }
-
     }
 }
+
+@Composable
+fun affirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
+
+    val context = LocalContext.current
+    val (showDialog, setShowDialog) = remember { mutableStateOf(false) }
+    val (selectedAffirmation, setSelectedAffirmation) = remember { mutableStateOf<Affirmation?>(null) }
+
+
+    LazyColumn(modifier = modifier) {
+        items(affirmationList) { affirmation ->
+            AffirmationCard(
+                affirmation = affirmation,
+                modifier = Modifier.padding(8.dp),
+
+                onClick = {
+                    Toast.makeText(context, context.getString(affirmation.stringResourceId), Toast.LENGTH_LONG).show()
+                    setSelectedAffirmation(affirmation)
+                    setShowDialog(true)
+                }
+            )
+        }
+    }
+
+    if (showDialog && selectedAffirmation != null) {
+        AffirmationDialog(
+            affirmation = selectedAffirmation,
+            onDismissRequest = {
+                setShowDialog(false)
+                setSelectedAffirmation(null)
+            }
+        )
+    }
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+//                Text(
+//                    text = context.getString(affirmation.stringResourceId),
+//                    style = MaterialTheme.typography.headlineSmall
+//                )
+    }
+}
+
 
 
 @Composable
@@ -135,6 +191,7 @@ fun AffirmationDialog(
         }
     }
 }
+
 
 
 @Preview
