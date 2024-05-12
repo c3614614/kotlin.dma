a29771a5688dd8c2276340ebadb565828ab87c3b
lk <lk@gmail.com>
2024-05-12 16:23:45 +0100

    added dependancies in buildgradle.kts

--- DessertClicker/build.gradle.kts
+++ DessertClicker/build.gradle.kts
@@ -19,3 +19,9 @@ plugins {
     id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
+buildscript {
+    ext {
+
+        lifecycle_version = "2.5.1" // Update this to the latest version if needed
+    }
+}
