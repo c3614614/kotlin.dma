83d672266ddde0e05f57416a332097495dc84057
lk <lk@gmail.com>
2024-05-16 16:03:20 +0100

    downloaded a new theme and added to theme.kt and color.kt also dadded a new font and changed the font to italiano_regular in type.kt

--- Woof/app/src/main/java/com/example/woof/ui/compose/Type.kt
+++ Woof/app/src/main/java/com/example/woof/ui/compose/Type.kt
@@ -1,56 +1,54 @@
-
 /*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ 
+Copyright (C) 2023 The Android Open Source Project*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+*
+https://www.apache.org/licenses/LICENSE-2.0
+*
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package com.example.woof.ui.compose
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import com.example.woof.R
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.example.woof.R
 
-val AbrilFatface = FontFamily(
-    Font(R.font.abril_fatface_regular)
+
+val italianno= FontFamily(
+    Font(R.font.italianno_regular)
 )
-
-val Montserrat = FontFamily(
-    Font(R.font.montserrat_regular),
-    Font(R.font.montserrat_bold, FontWeight.Bold)
+val caveat = FontFamily(
+    Font(R.font.caveat_regular),
+    Font(R.font.caveat_regular, FontWeight.Bold),
 )
-
 val Typography = Typography(
     displayLarge = TextStyle(
-        fontFamily = AbrilFatface,
+        fontFamily = italianno,
         fontWeight = FontWeight.Normal,
         fontSize = 36.sp
     ),
     displayMedium = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = caveat,
         fontWeight = FontWeight.Bold,
         fontSize = 20.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = caveat,
         fontWeight = FontWeight.Bold,
         fontSize = 14.sp
     ),
     bodyLarge = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = caveat,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp
     )
